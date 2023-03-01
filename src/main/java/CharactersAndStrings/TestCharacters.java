87ab36d8e7ee15ce689bb4f10fcf24892c23642f
abban <abban@LAPTOP-DT5QOMUN>
2023-03-01 17:52:16 +0200

    Creating a github

---
+++ src/main/java/CharactersAndStrings/TestCharacters.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CharactersAndStrings;
+
+/**
+ *
+ * @author abban
+ * Page 2
+ * Activity 2
+ */
+public class TestCharacters {
+    
+    public static void main(String[] args) {
+        char letter = Character.toUpperCase('b');
+        System.out.println(letter);
+        System.out.println(Character.toUpperCase('f'));
+    }
+    
+}
